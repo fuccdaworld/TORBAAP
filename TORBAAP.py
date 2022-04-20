cd0bfe1d453a4f7525e694785449ec3f6498e000
fuccdaworld <104100513+fuccdaworld@users.noreply.github.com>
2022-04-21 04:28:52 +0600

    Update TORBAAP.py

--- TORBAAP.py
+++ TORBAAP.py
@@ -483,18 +483,16 @@ imtiazak_ua_oppo    = 'Mozilla/5.0 (Linux; Android 5.1.1; A37f) AppleWebKit/537.
 imtiazak_ua_samsung = 'Mozilla/5.0 (Linux; Android 5.0; SM-G900P Build/LRX21T; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/43.0.2357.121 Mobile Safari/537.36 [FB_IAB/FB4A;FBAV/35.0.0.48.273;]'
 imtiazak_ua_windows = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36 [FBAN/EMA;FBLC/id_ID;FBAV/239.0.0.10.109;]'
 banner="""\033[1;37m
-       ###       ##    ## #### ##    ##  ######
-      ## ##      ##   ##   ##  ###   ## ##    ##
-     ##   ##     ##  ##    ##  ####  ## ##
-    ##     ##    #####     ##  ## ## ## ##   ####
-    #########    ##  ##    ##  ##  #### ##    ##
-    ##     ##    ##   ##   ##  ##   ### ##    ##
-    ##     ##    ##    ## #### ##    ##  ######
+       
+   _  _____   ___  ___  ____    
+  / |/ / _ | / _ )/ _ )/ __ \   
+ /    / __ |/ _  / _  / /_/ /   
+/_/|_/_/ |_/____/____/\____/    
+                                
  [*]=============================================
     Author   : Torbap
-    Facebook : https://facebook.com/Imtiaz.Aking
-    Team     : Somi + Zahid
-    Connect  : 03237528063
+    Facebook : https://facebook.com/saxonnahuel
+    Team     : sex
  [*]=============================================
    \033[1;31m  Use (Flight) Airplane Mod For Speed Up \033[1;37m
  [*]============================================="""
